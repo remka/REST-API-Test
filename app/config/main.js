eb27b7d1f924c9d2c78128229cbabab9355aac14
remka <remka@users.noreply.github.com>
2016-06-20 17:09:19 +0900

    again

--- app/config/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-'secret': 'MjY+Y$KFxw+Susc44_RM?N@6BN@F?t9N8&rNUkm*gQx*absqzXGmj^Ld=K8?t2C=LxxL-RkSad@bXT&U',
-'database': 'mongodb://remka:kik00l0l@jello.modulusmongo.net:27017/eJiru2dy'
-};
