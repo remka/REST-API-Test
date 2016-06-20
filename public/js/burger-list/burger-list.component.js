173b9c92ac03071c1c7b77f0b60b40edb546e9b5
remka <remuka@gmail.com>
2016-06-21 00:41:34 +0900

    More angular...

--- public/js/burger-list/burger-list.component.js
+++ public/js/burger-list/burger-list.component.js
@@ -3,32 +3,13 @@ angular.
   module('burgerList').
   component('burgerList', {
     templateUrl: 'js/burger-list/burger-list.template.html',
-    controller: function BurgerListController() {
-      this.burgers = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.',
-          age: 52
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.',
-          age: 10
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.',
-          age: 100
-        }, {
-          name: 'Un truc',
-          snippet: 'Oais ca va bon.',
-          age: 7
-        }, {
-          name: '浦島太郎',
-          snippet: '日本語でですか？',
-          age: 3
-        }
-      ];
+    controller: function BurgerListController($http) {
+      var self = this;
+      self.orderProp = 'age';
 
-      this.orderProp = 'age';
+      $http.get('data/burgers.json').then(function(response) {
+        self.burgers = response.data;
+      });
 
     }
 });
