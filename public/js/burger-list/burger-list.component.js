c1e1b8fa8fa50cebffd5272b9b8c35c7984a4f60
remka <remuka@gmail.com>
2016-06-21 00:33:55 +0900

    Still angular-ing...

--- public/js/burger-list/burger-list.component.js
+++ public/js/burger-list/burger-list.component.js
@@ -7,20 +7,28 @@ angular.
       this.burgers = [
         {
           name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
+          snippet: 'Fast just got faster with Nexus S.',
+          age: 52
         }, {
           name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 10
         }, {
           name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 100
         }, {
           name: 'Un truc',
-          snippet: 'Oais ca va bon.'
+          snippet: 'Oais ca va bon.',
+          age: 7
         }, {
           name: '浦島太郎',
-          snippet: '日本語でですか？'
+          snippet: '日本語でですか？',
+          age: 3
         }
       ];
+
+      this.orderProp = 'age';
+
     }
 });
