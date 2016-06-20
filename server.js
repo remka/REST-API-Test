4c38b70bd08976466a5a490637c8b4de36a9f9e4
remka <remka@users.noreply.github.com>
2016-06-20 17:37:44 +0900

    Allow x-origins.

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ mongoose.connection.on('open', function (err) {
     console.log('Connected to database.');
 });
 
+// Access-Control-Allow-Origin allowed for all domains
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
 
 // PASSPORT
 // =============================================================================
